83073d3b74c0d0f42e6c4c50550606531bbe1120
ashtonsears <ashtonesears@gmail.com>
2025-04-14 05:25:06 -0400

    fixed errors

--- server.js
+++ server.js
@@ -295,10 +295,11 @@ app.post("/api/sleep_symptoms", (req, res) => {
 
 const validateSymptom = (symptom) => {
     const schema = Joi.object({
+        _id: Joi.allow(""),
         symptom: Joi.string().min(3).max(50).required(),
         duration: Joi.number().integer().min(1).max(1440).required(),
         severity: Joi.number().integer().min(1).max(10).required(),
-        date: Joi.string().pattern(/^\d{2}\/\d{2}\/\d{4}$/).required(),
+        date: Joi.string().pattern(/^\d{4}\/\d{2}\/\d{2}$/).required(),
         time: Joi.string().pattern(/^\d{2}:\d{2}$/).required(),
         notes: Joi.string().max(500)
     });
