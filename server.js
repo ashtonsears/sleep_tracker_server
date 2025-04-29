ea9f37bb981645045b2256726a87f236591c6bb4
ashtonsears <ashtonesears@gmail.com>
2025-04-29 05:11:04 -0400

    fix

--- server.js
+++ server.js
@@ -350,7 +350,6 @@ app.post("/api/sleep_symptoms", upload.single("img"), async(req, res) => {
     console.log("req.file:", req.file);
     console.log("req.body:", req.body);
 
-    const { error } = sleepSymptomSchema.validate(req.body);
     if (error) {
         return res.status(400).send(`Validation error: ${error}`);
     }
